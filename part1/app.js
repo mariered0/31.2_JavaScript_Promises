33cc2553280536ff5cba79d79aa3dbd5c0691336
mariered0 <98713565+mariered0@users.noreply.github.com>
2022-09-03 13:32:20 -0700

    use image from API data and generate random number to make image tilted

--- part1/app.js
+++ part1/app.js
@@ -1,59 +1,55 @@
+//With promises
+
 url = 'http://numbersapi.com/'
-// header = {'Content-Type': 'application/json'}
 
-//Part 1 - #1
+//Part 1 - 
+//#1
 const favoriteNum = 29
-const favoriteNumFact = axios.get(`${url}${favoriteNum}/?json`)
-    .then((res) => console.log(res.data))
-    .catch((err) => console.log(err))
-
+$.getJSON(`${url}${favoriteNum}/?json`)
+    .then((data) => console.log(data))
 
 
 //#2
 
-const num1 = 10
-const num2 = 29
-const num3 = 19
-const num4 = 87
+let favNums = [10, 29, 19, 87]
 
-const divMultiNums = document.querySelector('#multi-nums');
-const pMultiNums = document.createElement('p');
-
-const multiNumFact = axios.get(`${url}${num1},${num2},${num3},${num4}/?json`)
-    .then((res) => {
-        const response = res.data
-        values = Object.values(response)
-        for (let el of values) {
-            pMultiNums.textContent += el + ' '
-            divMultiNums.append(pMultiNums)
-        }
-
+const multiNumFact = $.getJSON(`${url}${favNums}/?json`)
+    .then((data) => {
+        console.log(data);
     })
-    .catch((err) => console.log(err))
 
 //#3
-const favoriteNumPromises = []
+// const favoriteNumPromises = []
 const divMultiFavNums = document.querySelector('#multi-fav-nums')
-const pMultiFavNums = document.createElement('p');
+// const pMultiFavNums = document.createElement('p');
 
-for (let i = 1; i < 5; i++){
-    favoriteNumPromises.push(
-        axios.get(`${url}${favoriteNum}/?json`)
-    );
-}
+//Original
+// for (let i = 1; i < 5; i++){
+//     favoriteNumPromises.push(
+//         $.getJSON(`${url}${favoriteNum}/?json`)
+//     );
+// }
 
-Promise.all(favoriteNumPromises)
-    .then(favoriteNumFactArr => {
-        for (res of favoriteNumFactArr) {
-            const response = res.data.text
-            values = Object.values(response)
-            for (let el of values) {
-                pMultiFavNums.textContent += el
-                divMultiFavNums.append(pMultiFavNums)
-        }
-        }
+// Promise.all(favoriteNumPromises)
+//     .then(favoriteNumFactArr => {
+//         for (res of favoriteNumFactArr) {
+//             const response = res.data
+//             console.log(response);
+//             values = Object.values(response)
+//             for (let el of values) {
+//                 pMultiFavNums.textContent += el
+//                 divMultiFavNums.append(pMultiFavNums)
+//         }
+//         }
+//     })
+
+Promise.all(
+    Array.from({ length: 4 }, () => {
+        return $.getJSON(`${url}${favoriteNum}?json`);
     })
-    .catch(err => console.log(err));
+).then(facts => {
+    facts.forEach(data => $(divMultiFavNums).append(`<p>${data.text}</p>`))
+});
 
 
 
--- part2/app.js
+++ part2/app.js
@@ -1,45 +1,63 @@
+//Part 2
+
 const url = 'https://deckofcardsapi.com/api/deck/'
 let game = false;
 let deckId;
-let zIndex = 0; //Increment this every click
 
-async function processBtn(evt) {
+//#1
+$.getJSON(`${url}new/draw/`).then (data => {
+    const suit = data.cards[0].suit.toLowerCase();
+    const value = data.cards[0].value.toLowerCase();
+    console.log(data);
+    console.log('#1', `${value} of ${suit}`);
+});
+
+//#2
+let firstCard;
+$.getJSON(`${url}new/draw`)
+    .then(data => {
+        firstCard = data.cards[0];
+        deckId = data.deck_id;
+        return $.getJSON(`${url}${deckId}/draw/`);
+    })
+    .then(data => {
+        const secondCard = data.cards[0];
+        [firstCard, secondCard].forEach(function(card){
+            console.log('#2', `${card.value.toLowerCase()} of ${card.suit.toLowerCase()}`)
+        });
+    });
+
+//#3
+function processBtn(evt) {
     evt.preventDefault();
     // making request to shuffle the cards
     if (game == false){
-        const res = await axios.get(`${url}new/shuffle/?deck_count=1`)
+        $.getJSON(`${url}new/draw/?count=1`).then(data =>{
         game = true
-        console.log(res.data)
-        deckId = res.data.deck_id
-        console.log(deckId)
+        deckId = data.deck_id
+        })
     }
-    console.log('clicked')
     //making request to draw a card
-    const res = await axios.get(`${url}${deckId}/draw/?count=1`)
-    .then ((res) => {
-        console.log(res.data)
-        const suit = res.data.cards[0].suit.toLowerCase();
-        const value = res.data.cards[0].value
-        deckId = res.data.deck_id
-        console.log(deckId)
-        console.log('card', `${value} of ${suit}`)
-
-        showImg(value, suit);
+    $.getJSON(`${url}${deckId}/draw/?count=1`).then (data => {
+        showImg(data);
+        if (data.remaining == 0) $btn.remove();
     })
-    .catch ((err) => console.log(err));
     }
 
-
-function showImg (value, suit){
-    const img = document.createElement('img');
-    const imgDiv = document.querySelector('#img-div');
-    img.setAttribute('src', `cards/${value}_of_${suit}.png`)
-    img.classList.add(`${suit}`);
-    img.style.zIndex = zIndex;
-    imgDiv.append(img);
-    zIndex +=1;
+function showImg (data){
+    let img = data.cards[0].image;
+    let angle = Math.random() * 90 - 45;
+    let randomX = Math.random() * 40 - 20;
+    let randomY = Math.random() * 40 - 20;
+    const $imgDiv = $('#img-div');
+    $imgDiv.append($('<img>', {
+        src: img,
+        css: {
+            transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`
+        }
+    }))
 
 }
 
-const btn = document.querySelector('#btn');
-btn.addEventListener('click', processBtn);
+const $btn = $('#btn');
+$btn.on('click', processBtn);
